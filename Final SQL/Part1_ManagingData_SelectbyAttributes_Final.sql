520d257bfddb41f0b2c02fd8e2a8dbcdf9de9b66
jpbcali <jennypalomino@gmail.com>
2017-03-29 08:51:03 -0700

    Add files via upload

---
+++ Final SQL/Part1_ManagingData_SelectbyAttributes_Final.sql
@@ -0,0 +1,189 @@
+﻿/* To run a query, highlight the line(s) to include the ; and click on the Executy Query icon above (looks like a Play button). */
+
+/* Section 1 - Introduction to working with tables in PostgreSQL */
+/* Create a new table */
+/* Primary Key is the unique ID for each record in the table. It has a constraint that it must be unique for each record */
+/* varchar is a text field of variable length with the limit set by the number in the () */
+CREATE TABLE employees (
+	uid integer PRIMARY KEY,
+	firstname varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
+	department varchar(50) NOT NULL
+);
+
+/* Populate table with values */
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (1, 'Elizabeth', 'Smith', 'Public Health');
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (2, 'Edward', 'Jones', 'Transportation');
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (3, 'Kelly', 'Johnson', 'Education');
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (4, 'Thomas', 'Peters', 'Financial');
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (5, 'Patty', 'Jackson', 'Financial');
+
+/* If we try to assign a duplicate uid, we will get an error that the uid already exists*/
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (5, 'Mary', 'Anderson', 'Public Works');
+
+
+/* */
+/* Task #1: Add yourself to this table in the Education department */
+/* */
+INSERT INTO employees (uid, firstname, lastname, department) VALUES (6, 'Jenny', 'Palomino', 'Education');
+
+
+/* Get a count of the number of records in the table */
+SELECT count(*) from employees;
+
+/* View all records and columns in the table */
+SELECT * FROM employees;
+
+/* View specific columns only */
+SELECT lastname, department FROM employees;
+
+/* Sort the table values */
+/* DESC is used for descending order */
+SELECT * FROM employees order by firstname ASC;
+
+/* You can also limit the number of results that are returned by the query */
+SELECT * FROM employees order by firstname ASC limit 2;
+
+/* Select the unique values from a column */
+/* This is helpful so that you know what values can be used to query the data */
+SELECT distinct(department) FROM employees;
+
+/* Select the records that meet a certain criteria */
+SELECT * FROM employees where department = 'Financial';
+
+/* Count these records that meet a certain criteria */
+SELECT count(*) FROM employees where department = 'Financial';
+
+/* Or get a list of counts for all values of a column */
+SELECT department, count(*) FROM employees group by department;
+
+/* Then order the list of counts by department */
+SELECT department, count(*) FROM employees group by department order by department;
+
+/* Delete certain values from table */
+DELETE FROM employees WHERE department = 'Financial';
+
+/* Add new column to table */
+ALTER TABLE employees ADD officeloc varchar(50);
+
+/* Add data to this new column */
+UPDATE employees SET officeloc = '124 Mulford' WHERE department = 'Education';
+
+
+/* */
+/* Task #2: Add an office location to whoever is left in the table as either 103, 111, or 132 Mulford */
+/* Hint: you will first need to query the table to see who is left without a value for office */
+/* */
+select * from employees;
+UPDATE employees SET officeloc = '103 Mulford' WHERE department = 'Public Health';
+UPDATE employees SET officeloc = '132 Mulford' WHERE department = 'Financial';
+UPDATE employees SET officeloc = '111 Mulford' WHERE department = 'Transportation';
+
+
+/* */
+/* Task #3: Get a count of employees in each office location */
+/* Hint: you will need to use a group by clause along with the count records */
+/* */
+SELECT officeloc, count(*) FROM employees group by officeloc order by officeloc;
+
+
+/* Delete columns from tables */
+ALTER TABLE employees DROP officeloc;
+
+/* Delete the whole table */
+DROP TABLE employees;
+
+
+/* Section 2 - Loading Tabular Data into PostgreSQL */
+/* Create new table to store imported csv data */
+/* Remember that you are assigning the column names in this create query */
+
+CREATE TABLE lymedisease (
+	county varchar(50),
+	y2005 integer,
+	y2006 integer,
+    y2007 integer,
+    y2008 integer,
+    y2009 integer,
+    y2010 integer,
+    y2011 integer,
+    y2012 integer,
+    y2013 integer,
+    y2014 integer,
+	population integer,
+	incidence float
+);
+
+/* Import csv data */
+/* The WITH clause holds the options where we set the column delimiter and exclude the first row that has the headers */
+COPY lymedisease FROM '/home/ubuntu/Downloads/Spatial_Data_Management_with_PostgreSQL_PostGIS-master/csv/Lyme disease CA 05-14 report year.csv' WITH DELIMITER ',' HEADER CSV;
+
+
+/* */
+/* Task #4 - Query this table to find the record for Alameda county*/
+/* Hint: it could be helpful to query for the unique values in the county column so you can see how the county names are listed*/
+/* */
+SELECT distinct(county) from lymedisease order by county;
+SELECT * from lymedisease where county = 'Alameda';
+
+
+/* */
+/* Task #5 - Sort the table to find which county has the highest value in 2005 */
+/* Hint: you will need to sort the table using DESC */
+/* */
+SELECT * from lymedisease order by y2005 DESC;
+
+
+/* Query this table to see only counties that have at least one lyme disease case in 2005 */
+SELECT * from lymedisease where y2005 > 0;
+
+/* Get a count of the number of records that fill that criteria (only counties that have at least one lyme disease case in 2005)*/
+SELECT count(*) from lymedisease where y2005 > 0;
+
+/* Create a new column to label these counties (only counties that have at least one lyme disease case in 2005)*/
+ALTER TABLE lymedisease ADD y2005label varchar(50);
+
+
+/* */
+/* Task #6 - Modify y2005label depending on 2005 value */
+/* For counties that have at least one lyme disease case in 2005, set equal to value of "YES" */
+/* For counties that do NOT have at least one lyme disease case in 2005, set equal to value of "NO" */
+/* Hint: review your answer for Task #2 */
+/* */
+UPDATE lymedisease SET y2005label = 'YES' where y2005 > 0;
+UPDATE lymedisease SET y2005label = 'NO' where y2005 = 0;
+
+select * from lymedisease order by y2005label;
+
+/* Export the revised table to csv */
+COPY lymedisease TO '/home/ubuntu/Downloads/RevisedLymeDisease.csv' WITH DELIMITER ',' HEADER CSV;
+
+
+/* Section 3 - Managing User Access to Data */ 
+/* */
+/* Task #7 - Create a new user for yourself (ex: jpalomino) */
+/* Step 1: in the pgAdmin Object browser menu (left side of pgadmin), right-click on Login Roles and select New Login Role */
+/* Step 2: assign a Role name in the Properties tab and a password in the Defintion tab */
+/* */
+
+/* */
+/* Task #8 - Grant permissions to the new user by updating queries below */
+/* */
+/* Granting permissions or privileges: https://www.postgresql.org/docs/9.6/static/sql-grant.html */
+
+/* Grant certain permissions such as select and insert of new records to new user on table lymedisease */
+GRANT SELECT, INSERT ON lymedisease TO jpalomino;
+
+/* We can grant other permissions too to allow updating and deleting of records in the table */
+GRANT UPDATE, DELETE ON lymedisease TO jpalomino; 
+
+/* You can also specify the columns that these permissions apply to */
+/* GRANT SELECT (column_name), INSERT ON (columnname) ON lymedisease TO jpalomino;*/
+
+/* Or you can assign the permissions to the entire database */
+GRANT CONNECT ON DATABASE bootcamp TO jpalomino; 
+GRANT ALL PRIVILEGES ON DATABASE bootcamp TO jpalomino;
+
+
+/* For information about creating new users in pqsql: https://www.postgresql.org/docs/current/static/app-createuser.html */
+
---
+++ Part2_WritingSpatialQueries.sql
@@ -0,0 +1,176 @@
+﻿/* To run a query, highlight the line(s) to include the ; and click on the Executy Query icon above (looks like a Play button). */
+
+/* Section 2 - Writing Spatial Queries */
+
+/* First, let's do a quick review of the lessons learned for select by attributes */
+/* It can be helpful to check what version of PostgreSQL and PostGIS you are using, so that you reference documentation */
+select version();
+select PostGIS_full_version();
+
+/* Select first ten records from a table so you can see the table structure */
+select * from cnty_Lyme_disease
+limit 10; 
+
+/* Select all records in the table and sort ASCENDING for 2005 values*/
+select * from cnty_Lyme_disease
+order by "2005"; 
+
+
+/* */
+/* Task #1 - repeat the query to sort DESCENDING for 2014 */
+/* */ 
+/* Hint - http://www.postgresqltutorial.com/postgresql-order-by */
+
+
+/* Select all records that have a 0 for year 2005 */
+select * from cnty_Lyme_disease
+where "2005" = 0
+order by "2005"; 
+
+
+/* */
+/* Task #2 - Select all records that do NOT have a 0 for year 2014 */
+/* */ 
+/* Hint: https://www.postgresql.org/docs/9.5/static/functions-comparison.html*/
+
+
+
+/* Count how many counties have a 0 for 2008 */
+select count(*) from cnty_Lyme_disease
+where "2008" = 0; 
+
+/* */
+/* Task #3 - Count how many counties do NOT have a 0 for 2012 */
+/* */ 
+/* Hint: https://www.postgresql.org/docs/9.5/static/functions-comparison.html*/
+
+
+
+/* See which tables in database contain geometries */
+select * from geometry_columns;
+/* Note: rasters are not considered geometry */
+/* 3310 is for the projection called California Teale Albers, NAD 83, which has units = meters */
+
+/* See which tables are in raster format */
+select * from raster_columns;
+
+
+/* Now, the spatial fun can begin! */
+/* Select by Location Queries */
+
+/* Spatial Join */
+/* Create one table that joins the county name from cnty_Lyme_disease to each tick location */
+select cdph_ticks_albers.*, cnty_Lyme_disease.* 
+from cnty_Lyme_disease
+join cdph_ticks_albers 
+on ST_Contains (cnty_Lyme_disease.geom, cdph_ticks_albers.geom)
+order by cnty_Lyme_disease.county;
+
+
+/* */
+/* Task 4 - use the previous query to join the EPA ecoregion (epa_eco13_albers) to each tick location */
+/* */
+
+
+
+/* Spatial Join with Count */
+/* Count the number of tick locations in each county, sorted in descending order by count */
+select b.county, count(a.gid)
+from cdph_ticks_albers AS a 
+join cnty_Lyme_disease AS b 
+on ST_Contains (b.geom, a.geom)
+group by b.county
+order by count(a.gid) DESC;
+
+
+/* */
+/* Task 5 - Order the previous query by county in ASCENDING order */
+/* */
+/* Hint - http://www.postgresqltutorial.com/postgresql-order-by */
+
+/* Also, how many counties do NOT have any tick locations? */
+/* Hint - there are 58 counties in California, but how many records do you get for the Spatial Join with Count? */
+/* Answer - 58 counties minus 49 records, so 9 counties do NOT have any tick locations */
+
+
+/* */
+/* Task 6 - Count the number of tick locations in each EPA ecoregion (epa_eco13_albers), sorted in descending order by count */
+/* */
+/* Hint - you need to find a column in epa_eco13_albers that you can group the count by */
+
+
+
+/* Get the list of tick locations for Alameda county only */
+select * from cdph_ticks_albers, cnty_Lyme_disease
+where ST_INTERSECTS(cdph_ticks_albers.geom,
+cnty_Lyme_disease.geom)
+AND cnty_Lyme_disease.county = 'Alameda';
+
+
+/* */
+/* Task 7 - get the list of tick locations for EPA region called Central California Valley */ 
+/* */
+/* Hint - review the table epa_eco13_albers to see which columns contain the names of the regions */
+
+
+
+/*  We can also find all the CDPH tick locations within a certain distance of a point using ST_DWithin */
+/* Again, the units are meters */
+/* Berkeley (Long = -122.264653, Lat 37.872629) */
+select a.* 
+FROM cdph_ticks_albers as a 
+where ST_DWithin(a.geom, ST_Transform(ST_SetSRID(ST_MakePoint(-122.264653, 37.872629), 4326), 3310), 10000);
+
+
+/* Example of calculating area very quickly */
+/* Returns in SRID units: for California Teale Albers (ESPG 3310), this is meters */
+/* More info: http://postgis.net/docs/ST_Area.html */
+select county, Sum(ST_Area(geom))
+from cnty_Lyme_disease 
+group by county;
+
+
+/* We can also convert from sq meters to sq kilometers in the query 
+/* 1 sq kilo =	1,000,000 sq meters */
+/* ex: Alameda County where you are right now is 2,130 km2 according to Wikipedia */
+select county, Sum(ST_Area(geom))/1000000
+from cnty_Lyme_disease 
+group by county;
+
+
+
+/* Return to PDF instructions to move on to the next part of the exercise! */
+/* Save these queries below in the QGIS SQL window in the next part of the exercise! */
+
+/* We can dynamically buffer geometries with PostGIS using the ST_Buffer function */
+/* Again, the units are meters */
+select gid, ST_Buffer(geom, 5000) as geom FROM cdph_ticks_albers;
+
+/*  We can also find all the CDPH tick locations within a certain distance of a point using ST_DWithin */
+/* Berkeley (Long = -122.264653, Lat 37.872629) */
+select a.* FROM cdph_ticks_albers as a 
+where ST_DWithin(a.geom, ST_Transform(ST_SetSRID(ST_MakePoint(-122.264653, 37.872629), 4326), 3310), 10000);
+
+/* Find all tick locations within 10km of the Alameda County boundary */
+select a.* 
+from cdph_ticks_albers as a 
+where ST_DWithin(a.geom, (SELECT geom FROM cnty_lyme_disease WHERE county = 'Alameda'), 10000);
+
+/* Combine Buffer and Intersection with our user defined point to retrieve the spatial intersection of a 10km buffer around the Berkeley point within the county of Alameda */
+select a.gid, a.county, ST_Intersection(geom, ST_Buffer(ST_Transform(ST_SetSRID(ST_MakePoint(-122.264653, 37.872629), 4326), 3310), 10000)) as geom 
+from cnty_lyme_disease as a 
+where a.county = 'Alameda';
+
+
+/* More spatial query examples */
+/* http://postgis.net/docs/using_postgis_dbmanagement.html#examples_spatial_sql  */
+/* https://2015.foss4g-na.org/session/advanced-spatial-analysis-postgis.html */
+/* https://2015.foss4g-na.org/session/postgis-feature-frenzy.html */
+
+/* General Useful PostGIS Resources */
+/* Vector Data Management: http://postgis.net/docs/using_postgis_dbmanagement.html#idm2005 */
+/* Raster Data Management: http://postgis.net/docs/using_raster_dataman.html#RT_Raster_Loader */
+/* FAQ about rasters in PostGIS: http://postgis.net/docs/RT_FAQ.html */
+     
+
+
